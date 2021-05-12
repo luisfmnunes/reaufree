9cdf3698fa5094f9444dc082882723d824ff080a
Daniel Albuquerque <daniel.moura.developer@gmail.com>
2021-05-12 15:40:46 -0300

    add footer

--- index.php
+++ index.php
@@ -314,7 +314,9 @@ function redirectionTo($path)
             ?>
 
         </div>
-
+        <?php
+          include_once './utils/footer.php';
+        ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
 </body>
 
---
+++ utils/footer.php
@@ -0,0 +1,4 @@
+<footer class="p-2">
+  <p class="text-center">Este é um site sem vínculo com o Projeto Oficial do $REAU - VIRA-LATA FINANCE!</p>
+  <p class="text-center"><a href="https://cutt.ly/gbGqqBH" target="_blank"><i class="fas fa-paw"></i> Site Oficial</a> | <a href="https://cutt.ly/0bGqthc" target="_blank"> <i class="fab fa-github"></i> Github</a> | <a href="https://cutt.ly/ubGqoAs" target="_blank"> <i class="fab fa-wordpress"></i> Blog</a> </p>
+</footer>
